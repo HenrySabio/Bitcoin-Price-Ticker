252bdc965ead5aa4daaee831da76e49aa00fa3ca
Henry <hsabio.a@gmail.com>
2020-03-19 22:12:02 -0400

    Set up initial fetch from coindesk api

---
+++ js/coin.js
@@ -0,0 +1,18 @@
+// Defines API URL and default currency state
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+const priceTag = document.querySelector("h1")
+let currency = "USD";
+
+// Grabs current price from Coindesk API
+const checlPrice = () => {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            priceTag.innerHTML = data.bpi.USD.rate_float
+        })
+
+}
+
+// Runs Function on load
+checlPrice()
+
